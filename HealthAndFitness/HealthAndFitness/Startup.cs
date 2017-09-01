80d07494a72813f6c3d14d0014e3d7d2a47a909d
Brad Chapman <chapmanb86@gmail.com>
2017-09-01 11:55:52 -0600

    Change from visual studio

--- HealthAndFitness/HealthAndFitness/Startup.cs
+++ HealthAndFitness/HealthAndFitness/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -30,7 +26,7 @@ namespace HealthAndFitness
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("GitHub World!");
+                await context.Response.WriteAsync("Hello GitHub World!");
             });
         }
     }
